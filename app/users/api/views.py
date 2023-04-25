60f1530c9dabb545e200fd7bc56d99d2fdeb74c0
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2023-04-25 18:44:03 +0300

    Update the api docs for user endpoints

--- app/users/api/views.py
+++ app/users/api/views.py
@@ -2,6 +2,10 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+)
 from talks.models import Talk
 from talks.api.serializers import TalkSerializer
 from ..models import User
@@ -9,6 +13,23 @@ from .serializers import UserSerializer
 from .permissions import UserPermission
 
 
+@extend_schema_view(
+    retrieve=extend_schema(responses={200: UserSerializer, 404: None}),
+    update=extend_schema(
+        responses={200: UserSerializer, 401: None, 403: None, 404: None}
+    ),
+    partial_update=extend_schema(
+        responses={200: UserSerializer, 401: None, 403: None, 404: None}
+    ),
+    list_talks=extend_schema(
+        responses={
+            200: TalkSerializer(many=True),
+            401: None,
+            403: None,
+            404: None,
+        }
+    ),
+)
 class UserViewSet(
     mixins.RetrieveModelMixin, mixins.UpdateModelMixin, GenericViewSet
 ):
