2467b64107380e22189d0cdf26abb7c706c0baa7
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2023-07-04 23:33:20 +0300

    Support filtering by status in list user talks api

--- app/users/api/views.py
+++ app/users/api/views.py
@@ -5,7 +5,9 @@ from rest_framework.response import Response
 from drf_spectacular.utils import (
     extend_schema_view,
     extend_schema,
+    OpenApiParameter,
 )
+from drf_spectacular.types import OpenApiTypes
 from events.models import Event
 from events.api.serializers import EventSerializer
 from talks.models import Talk
@@ -48,11 +50,24 @@ class UserViewSet(
     permission_classes = [UserPermission]
     lookup_field = "username"
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="status",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.QUERY,
+                default=None,
+                description="Filter talk status (pending/approved/rejected)",
+            )
+        ],
+    )
     @action(detail=True, methods=["get"], url_path="talks")
     def list_talks(self, request, username):
-        # to do: support filtering talk status
         user = self.get_object()
-        queryset = Talk.objects.filter(speaker=user).all()
+        queryset = Talk.objects.filter(speaker=user)
+        status_param = request.query_params.get("status", None)
+        if status_param is not None:
+            queryset = queryset.filter(status=status_param)
         serializer = TalkSerializer(queryset, many=True)
         return Response(serializer.data)
 
