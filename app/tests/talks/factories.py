c4aac74944a134a1f18690018bfd4ebbffdaab37
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2023-08-16 23:02:14 +0300

    Test talk endpoints

--- app/tests/talks/factories.py
+++ app/tests/talks/factories.py
@@ -3,9 +3,6 @@ from talks.models import Talk
 from factory.django import DjangoModelFactory
 import factory
 from factory import fuzzy
-
-# from factory import Faker, fuzzy, SelfAttribute, LazyAttribute
-# from factory import SubFactory
 from tests.events.factories import EventFactory
 from tests.users.factories import UserFactory
 
@@ -18,23 +15,7 @@ class TalkFactory(DjangoModelFactory):
     speaker = factory.SubFactory(UserFactory)
     title = factory.Faker("text", max_nb_chars=60)
     start = factory.Faker("date_time", tzinfo=timezone.get_current_timezone())
-    # end = factory.Faker("date_time", tzinfo=timezone.get_current_timezone())
-    end = factory.Faker(
-        "date_time_between_dates",
-        tzinfo=timezone.get_current_timezone(),
-        datetime_start=factory.SelfAttribute("..start"),
+    end = factory.LazyAttribute(
+        lambda obj: obj.start + timezone.timedelta(hours=1)
     )
-    # end_date=lambda self: self.start + timezone.timedelta(minutes=30),
-    # )
-    # end = LazyAttribute(lambda obj: obj.start + timezone.timedelta(hours=1))
-    # end = LazyAttribute(lambda x: fuzzy.FuzzyDateTime("x.start").fuzz())
-    # end = fuzzy.FuzzyDateTime(start_dt=SelfAttribute("start"))
     status = fuzzy.FuzzyChoice(Talk.STATUSES, getter=lambda c: c[0])
-
-    # @factory.post_generation
-    # def schedule(self, create, extracted, **kwargs):
-    #     if not create:
-    #         return
-
-    #     if self.start > self.end:
-    #         self.start, self.end = self.end, self.start
---
+++ app/tests/talks/test_endpoints.py
@@ -0,0 +1,97 @@
+import pytest
+from django.urls import reverse
+from django.utils import timezone
+from tests.talks.factories import TalkFactory
+from tests.users.factories import UserFactory
+
+pytestmark = pytest.mark.django_db
+
+
+class TestTalkEndpoints:
+    @pytest.mark.parametrize(
+        "endpoint_name, method",
+        [
+            ("talk-detail", "put"),
+            ("talk-detail", "patch"),
+            ("talk-detail", "delete"),
+            ("talk-retrieve-stream-key", "get"),
+        ],
+    )
+    def test_when_not_authenticated_then_appropriate_endpoints_should_fail(
+        self, endpoint_name, method, send_request
+    ):
+        talk = TalkFactory.create()
+        url = reverse(endpoint_name, kwargs={"pk": talk.pk})
+        response = send_request(url, method)
+        assert response.status_code == 401
+
+    @pytest.mark.parametrize(
+        "endpoint_name, method",
+        [
+            ("talk-detail", "put"),
+            ("talk-detail", "patch"),
+            ("talk-detail", "delete"),
+            ("talk-retrieve-stream-key", "get"),
+        ],
+    )
+    def test_when_not_speaker_then_appropriate_endpoints_should_fail(
+        self, endpoint_name, method, send_request
+    ):
+        talk = TalkFactory.create(
+            start=timezone.now() + timezone.timedelta(days=1)
+        )
+        url = reverse(endpoint_name, kwargs={"pk": talk.pk})
+        response = send_request(url, method, user=UserFactory.create())
+        assert response.status_code == 403
+
+    @pytest.mark.parametrize(
+        "endpoint_name, method",
+        [
+            ("talk-detail", "put"),
+            ("talk-detail", "patch"),
+            ("talk-detail", "delete"),
+        ],
+    )
+    def test_when_talk_has_started_then_appropriate_endpoints_should_fail(
+        self, endpoint_name, method, send_request
+    ):
+        talk = TalkFactory.create(
+            start=timezone.now() - timezone.timedelta(minutes=1)
+        )
+        url = reverse(endpoint_name, kwargs={"pk": talk.pk})
+        response = send_request(url, method, user=UserFactory.create())
+        assert response.status_code == 403
+
+    @pytest.mark.parametrize("method", ["put", "patch"])
+    def test_when_talk_is_rejected_then_approve_talk_should_fail(
+        self, method, send_request
+    ):
+        talk = TalkFactory.create(
+            start=timezone.now() + timezone.timedelta(days=1),
+            status="rejected",
+        )
+
+        url = reverse("talk-detail", kwargs={"pk": talk.pk})
+        payload = {"status": "approved"}
+        response = send_request(url, method, payload, user=talk.speaker)
+        assert response.status_code == 400
+
+    @pytest.mark.parametrize("method", ["put", "patch"])
+    @pytest.mark.parametrize("new_status", ["approved", "rejected"])
+    def test_when_update_talk_status(self, method, new_status, send_request):
+        talk = TalkFactory.create(
+            start=timezone.now() + timezone.timedelta(days=1),
+            status="pending",
+        )
+
+        url = reverse("talk-detail", kwargs={"pk": talk.pk})
+        payload = {"status": new_status}
+        response = send_request(url, method, payload, user=talk.speaker)
+        assert response.status_code == 200
+
+    def test_retrieve_stream_key(self, send_request):
+        talk = TalkFactory.create()
+        url = reverse("talk-retrieve-stream-key", kwargs={"pk": talk.pk})
+        response = send_request(url, "get", user=talk.speaker)
+        assert response.status_code == 200
+        assert type(response.data["stream_key"]) is str
