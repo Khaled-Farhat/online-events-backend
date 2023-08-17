d913f8e54b6ec7d1839dae939f1c2960c57e8ca4
Khaled Farhat <khaled.farhat.cpp@gmail.com>
2023-08-17 14:52:40 +0300

    Display the talks in the event admin page

--- app/events/admin.py
+++ app/events/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Event
+from talks.models import Talk
+
+
+class TalkInline(admin.StackedInline):
+    model = Talk
 
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ["title", "organizer", "is_published"]
+    list_display = ["title", "organizer", "is_published", "started_at"]
+    inlines = [TalkInline]
